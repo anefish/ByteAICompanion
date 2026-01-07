bea8763fdcc7dce34cf9195b8f641214fdcf21b0
HuXiaofei 胡晓飞 <huxf36@cn.midea.com>
2026-01-07 19:04:33 +0800

    byte

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import Byte from "./components/Byte.vue";
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  </div> -->
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+  <Byte msg="" />
 </template>
 
 <style scoped>
---
+++ src/components/Byte.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+
+import { onMounted } from "vue";
+
+defineProps<{ msg: string }>();
+
+onMounted(() => {
+  setTimeout(() => {
+    window.location.href =
+      "https://x.com/i/grok?text=" +
+      encodeURIComponent("Woof, let me talk to Byte.");
+  }, 3000);
+});
+</script>
+
+<template>
+  <div class="fullscreen container">
+    <!-- <img class="" src="../assets/byte1.jpg" alt="" /> -->
+  </div>
+</template>
+
+<style scoped>
+.container {
+  background-image: url("../assets/byte1.jpg");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover; /* 关键：撑满容器，等比裁剪，不变形 */
+  /* background-attachment: fixed; 可选：固定背景，滚动时不动（视差效果） */
+}
+</style>
