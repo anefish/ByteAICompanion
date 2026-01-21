e0b03ada7a7e31232bfbd856da6b4eee4b06bdfb
HuXiaofei 胡晓飞 <huxf36@cn.midea.com>
2026-01-21 16:05:07 +0800

    init svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import byte1 from '$lib/assets/byte1.jpg';
+
+	// let { children } = $props();
+</script>
+
+<!-- <h1>Chat with Byte, the AI dog.</h1> -->
+<img src={byte1} alt="Byte" />
